3417ef32fc6a6effcf5e73700ac2ad55cf9fd0cd
danchikov_a <danchikovartem@gmail.com>
2022-01-24 13:16:01 +0300

    synchronized methods

--- src/main/java/com/es/phoneshop/model/product/ArrayListProductDao.java
+++ src/main/java/com/es/phoneshop/model/product/ArrayListProductDao.java
@@ -10,7 +10,7 @@ import java.util.stream.Collectors;
 public class ArrayListProductDao implements ProductDao {
     private long maxId = 1L;
     private List<Product> products;
-
+    private final Object lock = new Object();
 
     public ArrayListProductDao() {
         this.products = new ArrayList<>();
@@ -19,33 +19,41 @@ public class ArrayListProductDao implements ProductDao {
 
     @Override
     public Product getProduct(Long id) throws NoSuchElementException {
-        return products.stream()
-                .filter(product -> id.equals(product.getId()))
-                .findAny()
-                .orElseThrow(NoSuchElementException::new);
+        synchronized (lock) {
+            return products.stream()
+                    .filter(product -> id.equals(product.getId()))
+                    .findAny()
+                    .orElseThrow(NoSuchElementException::new);
+        }
     }
 
     @Override
     public List<Product> findProducts() {
-        return products.stream()
-                .filter(product -> product.getPrice() != null)
-                .filter(product -> product.getStock() > 0)
-                .collect(Collectors.toList());
+        synchronized (lock) {
+            return products.stream()
+                    .filter(product -> product.getPrice() != null)
+                    .filter(product -> product.getStock() > 0)
+                    .collect(Collectors.toList());
+        }
     }
 
     @Override
     public void save(Product product) {
-        if (product.getId() == null) {
-            product.setId(maxId++);
-            products.add(product);
-        } else {
-            products.set(Math.toIntExact(product.getId()),product);
+        synchronized (lock) {
+            if (product.getId() == null) {
+                product.setId(maxId++);
+                products.add(product);
+            } else {
+                products.set(Math.toIntExact(product.getId()), product);
+            }
         }
     }
 
     @Override
     public void delete(Long id) {
-        products.removeIf(product -> id.equals(product.getId()));
+        synchronized (lock) {
+            products.removeIf(product -> id.equals(product.getId()));
+        }
     }
 
     private void saveSampleProducts() {

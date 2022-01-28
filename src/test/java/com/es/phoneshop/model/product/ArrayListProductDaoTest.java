ebeca58715fbabc8d43d992a3c0c78e6d2597774
danchikov_a <danchikovartem@gmail.com>
2022-01-29 00:01:18 +0300

    added tests for searching products

--- src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
+++ src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
@@ -10,29 +10,38 @@ import java.util.Currency;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
-import static org.mockito.Mockito.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+
+
 
 @RunWith(MockitoJUnitRunner.class)
 public class ArrayListProductDaoTest {
-    private ProductDao productDao;
     private ProductDao productDaoSpy;
-
     private final String currencyUSD = "USD";
     private final String testCode = "test-code";
     private final String testDescription = "Samsung Galaxy S";
     private final String testImageURL = "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S.jpg";
+    private final String emptyString = "";
     private final int testStock = 100;
     private final int zeroStock = 0;
+    private final int amountOfSamsGalaxys = 2;
+    private final String descrToTestRightOrder = "Samsung Galaxy S III";
+    private final int firstElem = 0;
+    private final long idOfSamsGalaxyIII = 16L;
 
     @Before
     public void setup() {
-        productDao = new ArrayListProductDao();
-        productDaoSpy = spy(productDao);
+        productDaoSpy = spy(new ArrayListProductDao());
     }
 
     @Test
     public void shouldFindProductsNoResults() {
-        assertFalse(productDao.findProducts().isEmpty());
+        assertFalse(productDaoSpy.findProducts(anyString()).isEmpty());
     }
 
     @Test
@@ -46,8 +55,8 @@ public class ArrayListProductDaoTest {
                 .setStock(testStock)
                 .setImageUrl(testImageURL)
                 .build();
-        productDao.save(product);
-        assertNotNull(productDao.getProduct(product.getId()));
+        productDaoSpy.save(product);
+        assertNotNull(productDaoSpy.getProduct(product.getId()));
     }
 
     @Test
@@ -61,8 +70,8 @@ public class ArrayListProductDaoTest {
                 .setStock(zeroStock)
                 .setImageUrl(testImageURL)
                 .build();
-        productDao.save(product);
-        assertFalse(productDao.findProducts().contains(product));
+        productDaoSpy.save(product);
+        assertFalse(productDaoSpy.findProducts(anyString()).contains(product));
     }
 
     @Test(expected = NoSuchProductException.class)
@@ -89,5 +98,30 @@ public class ArrayListProductDaoTest {
                 .getProduct(null);
         productDaoSpy.getProduct(null);
     }
+
+    @Test
+    public void shouldFindAllProductsWhenQueryIsNull() {
+        productDaoSpy.findProducts(null);
+        verify(productDaoSpy).findProducts(null);
+    }
+
+    @Test
+    public void shouldFindAllProductsWhenQueryIsEmpty() {
+        productDaoSpy.findProducts(emptyString);
+        verify(productDaoSpy).findProducts(emptyString);
+    }
+
+    @Test
+    public void shouldFindProductsWithMatchingWordsInQuery() {
+        assertEquals(productDaoSpy.findProducts(testDescription).size(),amountOfSamsGalaxys);
+    }
+
+    @Test
+    public void shouldFindProductsInOrderOfMatchingWords() {
+        Product firstElement = productDaoSpy.findProducts(descrToTestRightOrder).get(firstElem);
+        System.out.println(productDaoSpy.findProducts(null));
+        assertEquals(firstElement, productDaoSpy.getProduct(idOfSamsGalaxyIII));
+    }
+
 
 }
